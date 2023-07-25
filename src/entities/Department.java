b9631c74ccc538f64f811948ff1850cb1caf2113
Lucas Silva <lucas.silva@branef.com.br>
2023-07-24 22:20:37 -0300

    first commit

---
+++ src/entities/Department.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class Department {
+	
+	private String name;
+	
+	public Department() {}
+	
+	public Department(String name) {
+		name = this.name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
